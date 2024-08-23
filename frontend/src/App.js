6b79f2464472c7d3fad40f663cf569bfa13fc7d1
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-08-23 15:50:03 -0700

    pc - add profile page to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "main/pages/HomePage";
+import ProfilePage from "main/pages/ProfilePage";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/profile" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
