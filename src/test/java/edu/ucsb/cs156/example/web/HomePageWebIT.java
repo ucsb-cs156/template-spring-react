1e79b7948da6b7bf33edf6b9d695cedd9a8a5a7b
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-08-23 12:29:48 -0700

    pc - adjust web it tests since we don't have a working frontend yet

--- src/test/java/edu/ucsb/cs156/example/web/HomePageWebIT.java
+++ src/test/java/edu/ucsb/cs156/example/web/HomePageWebIT.java
@@ -48,7 +48,10 @@ public class HomePageWebIT {
         String url = String.format("http://localhost:%d/", port);
         page.navigate(url);
 
-        assertThat(page.getByText("This is a webapp containing a bunch of different Spring Boot/React examples."))
+        // assertThat(page.getByText("This is a webapp containing a bunch of different Spring Boot/React examples."))
+        //         .isVisible();
+
+        assertThat(page.getByText("Learn React"))
                 .isVisible();
     }
 
