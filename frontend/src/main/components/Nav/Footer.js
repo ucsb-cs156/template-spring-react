8c946e3418d35e95bf83790a116f83b8a8056ed4
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-08-23 12:04:28 -0700

    pc - adjust test to test for presence of space, and add periodin footer text

--- frontend/src/main/components/Nav/Footer.js
+++ frontend/src/main/components/Nav/Footer.js
@@ -2,13 +2,13 @@ import { Container } from "react-bootstrap";
 
 export default function Footer() {
   return (
-    <footer className="bg-light pt-3 pt-md-4 pb-4 pb-md-5">
+    <footer className="bg-light pt-3 pt-md-4 pb-4 pb-md-5" data-testid="Footer">
       <Container>
         <p>
           This is a sample webapp using React with a Spring Boot backend. See
           the source code on{" "}
           <a href="https://github.com/ucsb-cs156/spring-react-template">
-            Github
+            Github.
           </a>
         </p>
       </Container>
