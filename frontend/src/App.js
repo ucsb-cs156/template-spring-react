33ad7f5034b4bfcea21abead9279a31b8bbe70ad
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-08-23 16:28:21 -0700

    pc - add AdminUsersPage which also means adding react-table and react-toastify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "main/pages/HomePage";
 import ProfilePage from "main/pages/ProfilePage";
+import AdminUsersPage from "main/pages/AdminUsersPage";
+
+import { hasRole, useCurrentUser } from "main/utils/currentUser";
+
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
+  const { data: currentUser } = useCurrentUser();
+
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/profile" element={<ProfilePage />} />
+        {hasRole(currentUser, "ROLE_ADMIN") && (
+          <Route exact path="/admin/users" element={<AdminUsersPage />} />
+        )}
       </Routes>
     </BrowserRouter>
   );
