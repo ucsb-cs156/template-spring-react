a872765c8007b52b1a575d65fb60d66694778420
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-08-24 17:16:33 -0700

    pc - fix stryker exception to account for npm format spreading code over two lines

--- frontend/src/main/components/UCSBDates/UCSBDateForm.js
+++ frontend/src/main/components/UCSBDates/UCSBDateForm.js
@@ -20,9 +20,10 @@ function UCSBDateForm({
   // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
   // Note that even this complex regex may still need some tweaks
 
-  // Stryker disable next-line Regex
+  // Stryker disable Regex
   const isodate_regex =
     /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+  // Stryker restore Regex
 
   // Stryker disable next-line all
   const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
